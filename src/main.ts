85aa12d4adaea43992f19a8fc4cb2c401b0341f6
BreezeBM <seyya77@gmail.com>
2023-06-25 14:22:48 +0900

    swagger 라이브러리 설치 및 연결

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('Color On Me API Docs')
+        .setDescription('컬러온미 API 문서 입니다!')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('api', app, document);
+
+    await app.listen(3000);
 }
 bootstrap();
