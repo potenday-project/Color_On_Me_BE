8334e284752442eb350129bada0d320205d28855
go-tiger <inter_tiger_@naver.com>
2023-06-26 05:32:29 +0900

    fix: 몽고 디비 연결 오류

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,8 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/local-color-on-me'),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/local-color-on-me')],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
