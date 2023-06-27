0b8b3fbb585c63cdba73a395452fe8b38802b799
BreezeBM <seyya77@gmail.com>
2023-06-27 14:29:01 +0900

    feat: findUserOrCreate, token값 업데이트 및 삭제 로직 작성

--- src/domain/user/user.module.ts
+++ src/domain/user/user.module.ts
@@ -8,5 +8,6 @@ import { User, UserSchema } from './model/user.model';
     imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
     controllers: [UserController],
     providers: [UserService],
+    exports: [UserService],
 })
 export class UserModule {}
